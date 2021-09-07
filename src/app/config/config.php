9c83b79667d319a801f7df2286f7217895d2b9d0
Yves Thirion <thirion.yves@gmail.com>
2021-09-07 10:50:57 +0200

    Created db manager and config.php
    DatabaseManager give an object to access the database from this class. Config.php define constants of the server, including the database credentials

---
+++ src/app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+    //Database params
+    define('DB_HOST','mysql');
+    define('DB_USER','root');
+    define('DB_PASS','root');
+    define('DB_NAME','forum-project');
+
+    //URLROOT (Dynamic links)
+    define('URLROOT','localhost');
+
+    //Sitename
+    define('SITENAME','Forum');
