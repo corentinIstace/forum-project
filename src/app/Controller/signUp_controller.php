62bfb2cc1c392a5fe9d3d62bde95e61fea18e063
corentinIstace <corentinistace1@gmail.com>
2021-09-07 16:41:45 +0200

    adding sugnUp controller function

--- src/app/Controller/signUp_controller.php
+++ src/app/Controller/signUp_controller.php
@@ -2,7 +2,38 @@
     require_once "../app/Model/sign_up.php";
     class SignUpController{
 
-        public function validation(){
+        public function check(){
 
+            global $error;
+            $error = [];
+
+                // ----- sanitize ----- //
+
+            $nickName = filter_var($_POST("nickname"), FILTER_SANITIZE_STRING);
+            $email = filter_var($_POST("email"), FILTER_SANITIZE_EMAIL);
+            $signature = filter_var($_POST("signature"), FILTER_SANITIZE_SPECIAL_CHARS);
+            $password = filter_var($_POST("password"), FILTER_SANITIZE_SPECIAL_CHARS);
+
+                // ----- Validate ----- //
+            if (!filter_var($email)){
+                add_error ("email", "This E-mail is invalid");
+            }
+
+            if (!filter_var($password, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^(?=.[A-Za-z])(?=.\d)[A-Za-z\d]{8,}$/")))){
+               add_error ("password", "Please secure your password with min 8 characters, Maj and numbers");
+            }
+            
+            function add_error($value, $message){
+                global $error;
+                $error[$value] = $message;
+            }
+            
+            if (count($error) > 0 ){
+                // require create error page
+            }
+            else {
+                require_once "../view/sign_in.php";
+            }
+                          
         }
     }
