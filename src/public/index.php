49cd433489a1d43df6e35c5edfb1f09c9ce0606d
Yves Thirion <thirion.yves@gmail.com>
2021-09-08 15:26:15 +0200

    Basic public index.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,13 @@
-<?php
-echo "Hello world<br><br>public/index";
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title><?php echo SITENAME ?></title>
+      <link rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>
+    <?php require_once '../app/index.php'; ?>
+  </body>
+  </html>
