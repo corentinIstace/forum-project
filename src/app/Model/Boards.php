eac37df013d827369d54989de55d98125e544f57
Yves Thirion <thirion.yves@gmail.com>
2021-09-09 18:20:57 +0200

    Update Boards.php model for editing
    Fetching data is now based on id. setBoard() and ediBoard() receives an array instead of an object. Added updateBoard() and editBoard()

--- src/app/Model/Boards.php
+++ src/app/Model/Boards.php
@@ -20,16 +20,15 @@ class Boards extends DatabaseManager
     return $data;
   }
 
-  private function fetchSingleBoard($name){
+  private function fetchSingleBoard($id){
     $db = $this->connectDb();
-
-    $req = $db->prepare("SELECT * FROM boards WHERE name=:name");
-    $req->execute(["name" => $name]);
-    return $req->fetch(PDO::FETCH_ASSOC);
+      $req = $db->prepare("SELECT * FROM boards WHERE id=:id");
+      $req->execute(["id" => $id]);
+      return $req->fetch(PDO::FETCH_ASSOC);
   }
 
-  public function getSingleBoard($name){
-    $data = $this->fetchSingleBoard($name);
+  public function getSingleBoard($id){
+    $data = $this->fetchSingleBoard($id);
     return $data;
   }
 
@@ -40,10 +39,24 @@ class Boards extends DatabaseManager
   }
 
   public function setBoard($board){
+    // Get only name and description, ignore empty id
     $board = [
-      'name' => $board->name,
-      'description' => $board->description
+      'name' => $board['name'],
+      'description' => $board['description']
     ];
     $this->insertBoard($board);
+  }
+
+  private function updateBoard($board){
+    $db = $this->connectDb();
+    $req = $db->prepare("");
+    $req = $db->prepare("UPDATE boards 
+                        SET name=:name, description=:description
+                        WHERE id=:id");
+    $req->execute($board);
+  }
+
+  public function editBoard($board){
+    $this->updateBoard($board);
   }
 }
