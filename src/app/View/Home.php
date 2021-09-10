13a8ece97d0717005e0581198a3239611a3e3f14
NaimZian <naimzian.dev@gmail.com>
2021-09-10 10:53:47 +0200

    bootstrap-homepage

--- src/app/View/Home.php
+++ src/app/View/Home.php
@@ -13,22 +13,32 @@
     <title>Seigneur Des Anneaux</title>
 </head>
 <header>
-    <section class="connexions-buttons">
-        <div class="register-button-div"></div>
-        <svg class="register-logo" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
-            <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
-            <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" />
-        </svg>
-        <button class="register-button">Register</button>
-        <div class="log-in-button-div">
-            <svg class="log-in-logo" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
-                <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
-            </svg></i><button class="log-in-button">Log in</button>
-        </div>
 
-    </section>
-    <H1 class="text-header">Tracker</H1>
+
+
+    <!-- Background image -->
+    <div class="mask gradient-custom">
+        <section class="connexions-buttons">
+            <div class="register-button-div"></div>
+            <svg class="register-logo" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
+                <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
+                <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" />
+            </svg>
+            <button class="register-button">Register</button>
+            <div class="log-in-button-div">
+                <svg class="log-in-logo" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+                    <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                </svg></i><button class="log-in-button">Log in</button>
+            </div>
+
+        </section>
+        <div class="d-flex justify-content-center align-items-center h-100">
+            <h1 class="text-white mb-0">The ring's Forum</h1>
+        </div>
+    </div>
+    </div>
+    <!-- Background image -->
 
 </header>
 
@@ -71,20 +81,34 @@
 
 </body>
 <footer>
-    <i class="fab fa-twitter"></i>
-    <i class="fab fa-apple"></i>
-    <i class="fab fa-facebook"></i>
-    <i class="fab fa-codepen"></i>
-    <i class="fab fa-google-plus-g"></i>
-    <i class="fab fa-digg"></i>
-    <i class="fab fa-pinterest"></i>
+    <section class="white-footer">
+        <!-- Grid container -->
+        <div class="container p-4">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+                <!-- Twitter -->
+                <a class="btn btn-outline-light btn-floating border rounded-circle m-1 " href="#!" role="button"><i class="fab fa-twitter"></i></a>
+                <!-- Apple -->
+                <a class="btn btn-outline-light btn-floating border rounded-circle m-1" href="#!" role="button"><i class="fab fa-apple"></i></a>
+                <!-- Facebook -->
+                <a class="btn btn-outline-light btn-floating border rounded-circle m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+                <!-- Code Pen -->
+                <a class="btn btn-outline-light btn-floating border rounded-circle m-1" href="#!" role="button"> <i class="fab fa-codepen"></i></a>
+                <!-- Google -->
+                <a class="btn btn-outline-light btn-floating border rounded-circle m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+                <!-- Digg -->
+                <a class="btn btn-outline-light btn-floating border rounded-circle m-1" href="#!" role="button"><i class="fab fa-digg"></i></a>
+                <!-- Pinterest -->
+                <a class="btn btn-outline-light btn-floating border rounded-circle m-1" href="#!" role="button"><i class="fab fa-pinterest"></i></a>
+            </section>
+    </section>
     <section class="black-footer">
-        <i class="fas fa-envelope"></i>
-        <i class="fas fa-shield-alt"></i>
-        <i class="fas fa-check"></i>
-        <i class="fas fa-lock"></i>
-        <i class="fas fa-users"></i>
-        <i class="fas fa-trash-alt"></i>
+        <i class="fas fa-envelope">Contact us</i>
+        <i class="fas fa-shield-alt">The team</i>
+        <i class="fas fa-check">Terms</i>
+        <i class="fas fa-lock">Privacy</i>
+        <i class="fas fa-users">Members</i>
+        <i class="fas fa-trash-alt">Delete cookies</i>
         <p>All times are UTC</p>
     </section>
     <section class="powered-by">
