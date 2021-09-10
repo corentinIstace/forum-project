4777913a5ec540dffd4779659ea209cf5edeb10a
Yves Thirion <thirion.yves@gmail.com>
2021-09-10 12:13:36 +0200

    Homepage basic mvc, simulate topics
    Homepage now is a class and call BoardController to get all boards. View simulate topics to display

--- src/app/Controller/HomepageController.php
+++ src/app/Controller/HomepageController.php
@@ -1,2 +1,16 @@
 <?php
-require "../app/View/Home.php";
+
+declare(strict_types=1);
+
+require_once '../app/Controller/BoardController.php';
+
+class HomepageController
+{
+  public function index()
+  {
+    // Get data from Boards, Topics
+    $boards = (new BoardController())->getAllBoards();
+
+    require "../app/View/Home.php";
+  }
+}
