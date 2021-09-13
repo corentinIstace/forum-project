f5cd656ef89de33ff17c425478d1bbbec0ce884b
Yves Thirion <thirion.yves@gmail.com>
2021-09-13 14:56:29 +0200

    Added reply topic url path

--- src/app/index.php
+++ src/app/index.php
@@ -24,6 +24,9 @@ switch ($page) {
   case 'topic':
     (new TopicController())->displayTopic();
     break;
+  case 'replytopic':
+    (new TopicController())->replyTopic();
+    break;
   case 'categories': // http://127.0.0.1/public/index.php?page=categories
     (new BoardController())->listBoards();
     break;
