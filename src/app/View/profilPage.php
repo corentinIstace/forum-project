5969de7f8af2097ad6800010366416236ed9b8be
corentinIstace <corentinistace1@gmail.com>
2021-09-15 16:03:32 +0200

    success to  catch data from the db

--- src/app/View/profilPage.php
+++ src/app/View/profilPage.php
@@ -1,25 +1,124 @@
+<?php
+    session_start(); 
+    require_once "../config/config.php";
+    require_once "../libraries/DatabaseManager.php";
+    
+    $id = $_SESSION['sess_user_id'];
+    $nickname = $_SESSION['sess_user_nickname'];
+    $email = $_SESSION['sess_user_email'];
+    $password = $_SESSION['sess_user_password'];
+    $signature = $_SESSION['sess_user_signature'];
+    
+    class UserSession extends DatabaseManager{
+        
+        public function changeData(){
+
+        }
+    }
+
+    $user = new UserSession();
+    $user -> changeData();
+
+
+
+
+
+
+        // if (isset($_POST['submit']))
+        // {
+        //     try {
+        //         $db = new PDO('mysql:host=mysql;dbname=forum-project;', 'root', 'root');
+        //     }
+        //     catch(PDOException $e){
+        //         echo $e -> getMessage();
+        //         exit();
+        //     }
+
+        //     $id = $_POST['id'];
+        //     $nickname = $_POST['nickname'];
+        //     $newNickname = $_POST['new_nickname'];
+
+        //     $query = ("UPDATE users set `nickname`=:nickname, `id`=:id");
+        //     $pdoResult = $db -> prepare($query);
+        //     $pdoResult -> execute( array(":nickname" => $newNickname,
+        //                                                 ":id"=> $id));
+        //     if ($pdoResult){
+        //         echo "<script>alert('data updated')</script>";    
+        //     }
+        //     else {
+        //         echo "error no update";
+        //     }
+               
+        // }
+        
+               
+    ?>
+
+
+
+
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>profil Page</title>
-</head>
-<body>
-
-
-
-
-<?php
-    session_start();
-    if(isset($_SESSION['sess_user_id']) && $_SESSION['sess_user_id'] != "") {
-      echo '<h1>Welcome '.$_SESSION['sess_name'].'</h1>';
-      echo '<h4><a href="logout.php">Logout</a></h4>';
-    } else { 
-      header('location:index.php');
-    }
-?>
-</body>
-</html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>profil Page</title>
+    </head>
+    <body>
+        <header>
+            <h1>Welcome <?= $nickname ?></h1>
+            <img src="" alt="img avatar">
+        </header>
+        <nav>
+            <ul>
+                <li><a href="../View/Home.php">Home</a></li>
+                <li><a href="#name_change">Nickname's change</a> </li>
+                <li><a href="#password_change">Password's change</a> </li>
+                <li><a href="#avatar_change">Avatar's change</a> </li>
+                <li><a href="#activities">Activities</a> </li>
+                <li>Logout</li>
+            </ul>
+        </nav>
+        <section id="data">
+            <h5>Your ID number is : <?= $id ?></h5>
+            <h5>Your nickname is : <?= $nickname ?></h5>
+            <h5>Your e-mail address is : <?= $email ?></h5>
+            <h5>Your password is : <?= $password ?></h5>
+            <h5>Your signature is : <?= $signature ?></h5>
+        </section>
+        <section id="name_change">
+            <h2>Change your Nickname:</h2>
+            <form method="post" action="profilPage.php">
+                <label for="id">ID</label>
+                <input type='number' name='id'><br><br>
+                <label for="nickname">Current nickname</label>
+                <input type='text' name='nickname'><br><br>
+                <label for="new_nickname">New nickname</label>
+                <input type='text' name='new_nickname'><br><br>
+                <input type='submit' name='change' value='Change Nickname'>
+            </form> 
+        </section>
+        
+        <section id="password_change">
+        <h2>Change your password:</h2>
+            <form method="post" action="profilPage.php">
+                <label for="password">Current password</label>
+                    <input type='text' name='password'><br><br>
+                <label for="new_password">New password</label>
+                    <input type='text' name='new_password'><br><br>
+                 <input type='submit' name='change' value='Change password'>
+            </form> 
+        </section>
+        
+        <section id="avatar_change">
+        <h2>Change your avatar:</h2>
+        </section>
+        
+        <section id="activities">
+        <h2>Your Activities:</h2>
+        
+        </section>
+        </body>
+        </html>
--- src/app/View/sign_in.php
+++ src/app/View/sign_in.php
@@ -1,44 +1,57 @@
 <?php
+  session_start();
+  $_SESSION['user_session'] = $userSession;
 
-try {
-  $db = new PDO('mysql:host=mysql;dbname=forum-project;', 'root', 'root');
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);  
-} catch (PDOException $e) {
-  echo "Connection failed : ". $e->getMessage();
-}
+  require_once "../config/config.php";
+  require_once "../libraries/DatabaseManager.php";
 
-if(isset($_POST['submit'])) {
-  $email = trim($_POST['email']);
-  $password = trim($_POST['password']);
-  if($email != "" && $password != "") {
-    try {
-      $query = "select nickname from `users` where `email`=:email and `password`=:password";
-      $stmt = $db->prepare($query);
-      $stmt->bindParam('email', $email, PDO::PARAM_STR);
-      $stmt->bindValue('password', $password, PDO::PARAM_STR);
-      $stmt->execute();
-      $count = $stmt->rowCount();
-      $row   = $stmt->fetch(PDO::FETCH_ASSOC);
-      if($count == 1 && !empty($row)) {
-        /******************** Your code ***********************/
-        
-        header("Location:../View/profilPage.php");
-        // echo "Bonjour à vous ";
-        // print_r($row['nickname']);
-        //  require_once "../View/profilPage.php";
-        
-      } else {
-        echo "Invalid username or password or creating acount";
+  class SignIn extends DatabaseManager{
+
+    public function connexion(){
+
+      if(isset($_POST['submit'])) {
+        $email = trim($_POST['email']);
+        $password = trim($_POST['password']);
+        if($email != "" && $password != "") {
+          try {
+            $db = $this->connectDb();
+            $query = "select * from `users` where `email`=:email and `password`=:password";
+            $stmt = $db->prepare($query);
+            $stmt->bindParam('email', $email, PDO::PARAM_STR);
+            $stmt->bindValue('password', $password, PDO::PARAM_STR);
+            $stmt->execute();
+            $count = $stmt->rowCount();
+            $row   = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if($count == 1 && !empty($row)) {
+              /******************** Your code ***********************/
+              $_SESSION['sess_user_id']   = $row['id'];
+              $_SESSION['sess_user_nickname'] = $row['nickname'];
+              $_SESSION['sess_user_email'] = $row['email'];
+              $_SESSION['sess_user_password'] = $row['password'];
+              $_SESSION['sess_user_signature'] = $row['signature'];
+
+              header("Location:../View/profilPage.php");
+              // echo "Bonjour à vous ";
+              // print_r($row['nickname']);
+              //  require_once "../View/profilPage.php";
+              
+            } else {
+              echo "Invalid username or password or creating acount";
+            }
+          } catch (PDOException $e) {
+            echo "Error : ".$e->getMessage();
+          }
+        } else {
+          echo "Both fields are required!";
+        }
       }
-    } catch (PDOException $e) {
-      echo "Error : ".$e->getMessage();
     }
-  } else {
-    echo "Both fields are required!";
   }
-}
-?>
+
+  $signIn = new SignIn();
+  $signIn -> connexion();
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
