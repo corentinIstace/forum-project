3222505b9846fc82a6c574be5467140445afed0a
Yves Thirion <thirion.yves@gmail.com>
2021-09-16 21:42:13 +0200

    Moved class SignIn to SignIn.php model file

--- src/app/View/sign_in.php
+++ src/app/View/sign_in.php
@@ -1,52 +1,5 @@
 <?php
-  session_start();
-
-  require_once "../app/config/config.php";
-  require_once "../app/libraries/DatabaseManager.php";
-
-  class SignIn extends DatabaseManager{
-
-    public function connexion(){
-
-      if(isset($_POST['submit'])) {
-        $email = trim($_POST['email']);
-        $password = trim($_POST['password']);
-        if($email != "" && $password != "") {
-          try {
-            $db = $this->connectDb();
-            $query = "select * from `users` where `email`=:email and `password`=:password";
-            $stmt = $db->prepare($query);
-            $stmt->bindParam('email', $email, PDO::PARAM_STR);
-            $stmt->bindValue('password', $password, PDO::PARAM_STR);
-            $stmt->execute();
-            $count = $stmt->rowCount();
-            $row   = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            if($count == 1 && !empty($row)) {
-              /******************** Your code ***********************/
-              $_SESSION['user_id']   = $row['id'];
-              $_SESSION['user_nickname'] = $row['nickname'];
-              $_SESSION['user_email'] = $row['email'];
-              $_SESSION['user_password'] = $row['password'];
-              $_SESSION['user_signature'] = $row['signature'];
-
-              header("Location:../app/index.php?page=profile");
-              // echo "Bonjour à vous ";
-              // print_r($row['nickname']);
-              //  require_once "../View/profilPage.php";
-              
-            } else {
-              echo "Invalid username or password or creating acount";
-            }
-          } catch (PDOException $e) {
-            echo "Error : ".$e->getMessage();
-          }
-        } else {
-          echo "Both fields are required!";
-        }
-      }
-    }
-  }
+  require_once "../app/Model/SignIn.php";
 
   $signIn = new SignIn();
   $signIn -> connexion();
@@ -63,12 +16,12 @@
     <h1>Connexion:</h1>
 
     <form action="" method="POST">
-            <input type="file" name="avatar" accept="image/png, image/jpeg">
-    <hr width="25%">
+        <input type="file" name="avatar" accept="image/png, image/jpeg">
+        <hr width="25%">
         <label for="email">E-mail</label>
-            <input type="email" name="email" value="">
+        <input type="email" name="email" value="">
         <label for="password">Password</label>
-            <input type="password" name="password" value="">
+        <input type="password" name="password" value="">
         <input type="submit" name="submit" value="Connexion.">
     </form>
 </body>
