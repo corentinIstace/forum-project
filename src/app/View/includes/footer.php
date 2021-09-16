17321a5afc350a38568e3d64aff21f22ae0e60a9
hawkstan <zoe.ranzy@gmail.com>
2021-09-16 16:52:23 +0200

    separated footer & header

---
+++ src/app/View/includes/footer.php
@@ -0,0 +1,61 @@
+<footer>
+    <section class="white-footer">
+        <!-- Grid container -->
+        <div class="container p-4">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+              <!-- Twitter -->
+              <a class="btn btn-outline-light btn-floating border rounded-circle m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+              <!-- Apple -->
+              <a class="btn btn-outline-light btn-floating border rounded-circle m-1" href="#!" role="button"><i class="fab fa-apple"></i></a>
+              <!-- Facebook -->
+              <a class="btn btn-outline-light btn-floating border rounded-circle m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+              <!-- Code Pen -->
+              <a class="btn btn-outline-light btn-floating border rounded-circle m-1" href="#!" role="button"> <i class="fab fa-codepen"></i></a>
+              <!-- Google -->
+              <a class="btn btn-outline-light btn-floating border rounded-circle m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+              <!-- Digg -->
+              <a class="btn btn-outline-light btn-floating border rounded-circle m-1" href="#!" role="button"><i class="fab fa-digg"></i></a>
+              <!-- Pinterest -->
+              <a class="btn btn-outline-light btn-floating border rounded-circle m-1" href="#!" role="button"><i class="fab fa-pinterest"></i></a>
+            </section>
+        </div>
+    </section>
+    <section class="black-footer">
+      <div class="black-footer-ariane">
+        <nav class="breadcrumbF" ;aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Board Index</li>
+          </ol>
+        </nav>
+      </div>
+      <div class="black-footer-logos">
+        <a href="#">
+          <i class="fas fa-envelope"><p class="paragraph ml-3 ">Contact us</p></i>
+        </a>
+        <a href="#">
+          <i class="fas fa-shield-alt"><p class="paragraph ml-3 ">The Team</p></i>
+        </a>
+        <a href="#">
+          <i class="fas fa-check"><p class="paragraph ml-3 ">Terms</p></i>
+        </a>
+        <a href="#">
+          <i class="fas fa-lock"><p class="paragraph ml-3 ">Privacy</p></i>
+        </a>
+        <a href="#">
+          <i class="fas fa-users"><p class="paragraph ml-3 ">Members</p></i>
+        </a>
+        <a href="#">
+          <i class="fas fa-trash-alt"><p class="paragraph ml-3 ">Delete cookies</p></i>
+        </a>
+        <p>All times are UTC</p>
+      </div>
+    </section>
+    <section class="powered-by">
+      <a href="">Powered by <span class="couchpotatoes">Couch Potatoes</span> </a>
+    </section>
+    <button type="button" class="btn btn-danger btn-floating btn-lg" id="btn-back-to-top">
+      <i class="fas fa-arrow-up"></i>
+    </button>
+</footer>
