d4831e19d24eadb55cb9a9eed5901423b69385ac
Yves Thirion <thirion.yves@gmail.com>
2021-09-17 14:32:25 +0200

    Created sigle category view

---
+++ src/app/View/boards/board.php
@@ -0,0 +1,12 @@
+<?php
+  /* Loop on topics of the board to display them */
+?>
+  <section>
+    <h3><a href="../app/index.php?page=category&id=<?= $board['id'] ?>"><?= $board['name'] ?></a></h3>
+    <i><?= $board['description'] ?></i>
+    <?php foreach ($topics as $topic): ?>
+      <article>
+        <a href="../app/index.php?page=topic&id=<?= $topic['id'] ?>"><?= $topic['title'] ?> by <?= $topic['author_id'] ?> at <?= $topic['creation_date'] ?></a>
+      </article>
+    <?php endforeach; ?>
+  </section>
