cfbbb3d458cba09ae745ad4a7e36fe8ec861678a
Yves Thirion <thirion.yves@gmail.com>
2021-09-18 20:31:59 +0200

    Fixed sql call, cleanup comments

--- src/app/Model/User.php
+++ src/app/Model/User.php
@@ -12,49 +12,44 @@
     }
     
     public function changeNickname(){
-
           if(isset($_POST['changeName'])){
-
               // connect to the bookdb database
               $db = $this->connectDb();
               $data = [
-                  ':id' => $_SESSION['user_id'],
-                  ':nickname' => $_POST['new_nickname']
+                  'id' => $_SESSION['user_id'],
+                  'nickname' => $_POST['new_nickname']
               ];
               $sql = 'UPDATE users SET nickname = :nickname WHERE id = :id'; 
               // prepare statement
               $statement = $db->prepare($sql);
               // execute the UPDATE statment
               if ($statement->execute($data)) {
-                  $sql = 'SELECT nickname = :nickname FROM users WHERE id = :id'; 
+                  $sql = 'SELECT nickname FROM users WHERE id = :id'; 
                   $statement = $db->prepare($sql);
                   $statement->execute($data);
                   $_SESSION['user_nickname'] = $_POST['new_nickname'];
                   header("Location:../app/index.php?page=profile");
-                  // echo 'The name has been updated successfully!';
               }   
           }
       }
       public function changePassword(){
           if(isset($_POST['changePassword'])){
-
               // connect to the bookdb database
               $db = $this->connectDb();
               $data = [
-                  ':id' => $_SESSION['user_id'],
-                  ':password' => $_POST['new_password']
+                  'id' => $_SESSION['user_id'],
+                  'password' => $_POST['new_password']
               ];
               $sql = 'UPDATE users SET password = :password WHERE id = :id'; 
               // prepare statement
               $statement = $db->prepare($sql);
               // execute the UPDATE statment
               if ($statement->execute($data)) {
-                  $sql = 'SELECT password = :password FROM users WHERE id = :id'; 
+                  $sql = 'SELECT password FROM users WHERE id = :id'; 
                   $statement = $db->prepare($sql);
                   $statement->execute($data);
                   $_SESSION['user_password'] = $_POST['new_password'];
                   header("Location:../app/index.php?page=profile");
-                  // echo 'The password has been updated successfully!';
               }   
           }
       }
