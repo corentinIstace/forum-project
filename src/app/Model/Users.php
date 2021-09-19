e1db513b24b2e079b770b6f55b60f70059b83989
corentinIstace <corentinistace1@gmail.com>
2021-09-19 14:10:48 +0200

    scss for sign_in and profiPage

--- src/app/Model/Users.php
+++ src/app/Model/Users.php
@@ -6,12 +6,6 @@ require_once '../app/libraries/DatabaseManager.php';
 // Class extending dbManager to define access for the users table
 class Users extends DatabaseManager
 {
-  public function getProfileUser()
-  {
-    
-  }
-
-
   public function changeNickname(){
     if(isset($_POST['changeName'])){
       // connect to the bookdb database
--- src/app/View/users/sign_up.php
+++ src/app/View/users/sign_up.php
@@ -25,9 +25,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     -->
   <header>
-
-
-
+    
     <!-- Background image -->
     <div class="mask gradient-custom">
         <div class="d-flex justify-content-center align-items-center h-100">
